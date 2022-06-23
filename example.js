952a72b750870d191df675ec78933ce595841213
iymuan <iymuan92@gmail.com>
2022-06-23 10:35:10 +0100

    first commit

---
+++ example.js
@@ -0,0 +1,5 @@
+function profile(fullName, height, country) {
+    return `My name is ${fullName}. I am a ${height} tall ${country}. I am very pleased to be part of this cohort.`;}
+
+    console.log(profile("okechukwu", "6'5ft", "Nigerian",));
+    document.write(profile("okechukwu", "6'5ft", "Nigerian",))
